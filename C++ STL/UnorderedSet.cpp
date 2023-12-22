2648d4de65483dbd99e6f612b2982e5f22344dc8
srgamansrg <aman.srg234@gmail.com>
2023-12-23 04:02:32 +0530

    23rd December

---
+++ C++ STL/UnorderedSet.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainUSet() {
+    // all same as set 
+    // unique but not sorted
+    unordered_set<int> st;
+    // lower bound and upper bound function 
+    // does not works, rest all functions are same
+    //as above it does does not store in any 
+    // particular order it has a better complexity
+    // than set in most cases, 
+    //except some when collision happens
+    // time complexity is O(1)
+    // but worst case (Almost never happens) O(N)
+}
+
+int main() {
+
+    return 0;
+}
